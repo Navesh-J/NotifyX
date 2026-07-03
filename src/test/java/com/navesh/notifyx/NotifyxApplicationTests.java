1af4c89219161012ca201539968d897adcb92d80
Navesh Jaiswal <navesh.professional@gmail.com>
2026-07-03 19:47:33 +0530

    project setup

---
+++ src/test/java/com/navesh/notifyx/NotifyxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navesh.notifyx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifyxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
